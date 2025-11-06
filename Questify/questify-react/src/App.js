c96a04228aa2df672acd3ff927bd3d82ec530662
jxmasx <jonmasx@gmail.com>
2025-11-06 12:40:48 -0500

    switch to hashrouter
    apparently browser router requires server side running whereas hashrouter doesn't so i'm giving this a try

--- Questify/questify-react/src/App.js
+++ Questify/questify-react/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
 import Landing from "./components/Landing";
 import Login from "./components/Login";
