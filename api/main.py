1e26caad65b1ef8d809feafe8615a8fcb1775465
jxmasx <jonmasx@gmail.com>
2025-11-06 20:41:46 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -33,8 +33,7 @@ if __name__ == "__main__":
     import traceback
 
     try:
-        uvicorn.run(API, host="0.0.0.0", port=5000,
-                    ssl_keyfile="key.pem", ssl_certfile="cert.pem",)
+        uvicorn.run(API, host="0.0.0.0", port=5000)
     except Exception as e:
         print("\nError occurred:")
         traceback.print_exc()
